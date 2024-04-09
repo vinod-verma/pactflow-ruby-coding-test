8871fab6dd771c3c6b7af6fbb574e14ebf2a8fb0
vinod-verma <vi.vermad@gmail.com>
2024-04-09 18:58:06 +0530

    feature_spec: manage tie break condition

--- spec/tennis_tournament_rspec.rb
+++ spec/tennis_tournament_rspec.rb
@@ -71,4 +71,28 @@ RSpec.describe TennisTournament do
       end
     end
   end
+
+  describe "#tie_break" do
+    before(:all) do
+      @match = TennisTournament.new
+      @match.player1_games = 6
+      @match.player2_games = 5
+    end
+
+    context "when tie_break condition arrived" do
+      it "increments player2's points" do
+        4.times { @match.point_won_by("player2") }
+        expect(@match.score).to eq("6-6, 0-0")
+        expect(@match.tiebreak).to eq(true)
+      end
+    end
+
+    context "when player1 win in tie_break" do
+      it "increments player2's points" do
+        7.times { @match.point_won_by("player1") }
+        expect(@match.score).to eq("7-6, 0-0")
+        expect(@match.tiebreak).to eq(false)
+      end
+    end
+  end
 end
