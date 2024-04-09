7e6c1227d4d22ae858a7e54ca0e25e39f2138b24
vinod-verma <vi.vermad@gmail.com>
2024-04-09 19:31:09 +0530

    feature: manage tie break condition

--- solution/tennis_game.rb
+++ solution/tennis_game.rb
@@ -5,7 +5,6 @@ module TennisGame
     else
       @player2_points += 1
     end
-
     if @player1_points >= 4 && @player1_points >= @player2_points + 2
       @player1_games += 1
       reset_points
@@ -16,11 +15,36 @@ module TennisGame
       @player1_points = 3
       @player2_points = 3
     end
+
+    if @player1_games == 6 && @player2_games == 6
+      @tiebreak = true
+    end
+  end
+
+  def update_tiebreak(player)
+    if player == "player1"
+      @tiebreak_points[0] += 1
+    else
+      @tiebreak_points[1] += 1
+    end
+
+    if @tiebreak_points[0] >= 7 && @tiebreak_points[0] >= @tiebreak_points[1] + 2
+      @player1_games += 1
+      reset_tiebreak
+    elsif @tiebreak_points[1] >= 7 && @tiebreak_points[1] >= @tiebreak_points[0] + 2
+      @player2_games += 1
+      reset_tiebreak
+    end
   end
 
   def reset_points
     @player1_points = 0
     @player2_points = 0
+  end
+
+  def reset_tiebreak
+    @tiebreak = false
+    @tiebreak_points = [0, 0]
   end
 
   def game_score(points)
--- solution/tennis_tournament.rb
+++ solution/tennis_tournament.rb
@@ -1,15 +1,23 @@
 require_relative 'tennis_game'
 class TennisTournament
   include TennisGame
+  attr_accessor :player1_games, :player2_games
+  attr_reader :tiebreak
   def initialize
     @player1_points = 0
     @player2_points = 0
     @player1_games = 0
     @player2_games = 0
+    @tiebreak = false
+    @tiebreak_points = [0, 0]
   end
 
   def point_won_by(player)
-    update_game(player)
+    if @tiebreak
+      update_tiebreak(player)
+    else
+      update_game(player)
+    end
   end
 
   def score
